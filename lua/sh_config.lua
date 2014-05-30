57b7d4c3143c996c66cf7a8ebd87a49cbbf7f81e
EliteTK <tomasz.kramkowski@gmail.com>
2014-05-30 15:42:17 +0000

    Removed unwanted things.

--- lua/sh_config.lua
+++ lua/sh_config.lua
@@ -14,8 +14,8 @@ PS.Config.ShopChatCommand = '!shop' -- Chat command to open the shop, set to bla
 PS.Config.NotifyOnJoin = true -- Should players be notified about opening the shop when they spawn?
 
 PS.Config.PointsOverTime = true -- Should players be given points over time?
-PS.Config.PointsOverTimeDelay = 1 -- If so, how many minutes apart?
-PS.Config.PointsOverTimeAmount = 10 -- And if so, how many points to give after the time?
+PS.Config.PointsOverTimeDelay = 5 -- If so, how many minutes apart?
+PS.Config.PointsOverTimeAmount = 1 -- And if so, how many points to give after the time?
 
 PS.Config.AdminCanAccessAdminTab = true -- Can Admins access the Admin tab?
 PS.Config.SuperAdminCanAccessAdminTab = true -- Can SuperAdmins access the Admin tab?
@@ -23,7 +23,7 @@ PS.Config.SuperAdminCanAccessAdminTab = true -- Can SuperAdmins access the Admin
 PS.Config.CanPlayersGivePoints = true -- Can players give points away to other players?
 PS.Config.DisplayPreviewInMenu = true -- Can players see the preview of their items in the menu?
 
-PS.Config.PointsName = 'Points' -- What are the points called?
+PS.Config.PointsName = 'Potatoes' -- What are the points called?
 PS.Config.SortItemsBy = 'Name' -- How are items sorted? Set to 'Price' to sort by price.
 
 -- Edit below if you know what you're doing
